eb381fbd891e778cc049b847de07aaddc51107d5
michaelbeset-ops <michael.beset@gmail.com>
2026-04-05 20:46:03 +0200

    Add files via upload

---
+++ make.bat
@@ -0,0 +1,82 @@
+{
+    "opdrachtgevers": [
+        {
+            "naam": "Test",
+            "plaatsen": [
+                {
+                    "naam": "Zoetermeer",
+                    "wijken": ["Oosterheem", "Noordhoven"]
+                }
+            ]
+        },
+        {
+            "naam": "Lansingerland",
+            "plaatsen": [
+                {
+                    "naam": "Berkel & Rodenrijs",
+                    "wijken": ["Noord", "Zuid"]
+                },
+                {
+                    "naam": "Bergschenhoek",
+                    "wijken": ["Noord", "Zuid"]
+                },
+                {
+                    "naam": "Bleiswijk",
+                    "wijken": ["Noord", "Zuid"]
+                }
+            ]
+        },
+        {
+            "naam": "Leiden",
+            "plaatsen": [
+                {
+                    "naam": "Leiden",
+                    "wijken": ["Binnenstad", "Stationdistrict", "Noord", "Roodenburgerdistrict", "Bos- en Gasthuisdistrict", "Morsdistrict", "Boerhaavedisctrict", "Merenwijk", "Stevenshof"]
+                }
+            ]
+        },
+        {
+            "naam": "Maassluis",
+            "plaatsen": [
+                {
+                    "naam": "Maassluis",
+                    "wijken": ["Burgemeesterwijk", "Vogelwijk", "Oranjewijk", "Zwembadwijk", "Molenbuurt", "Kapelpolder"]
+                }
+            ]
+        },
+        {
+            "naam": "Westland",
+            "plaatsen": [
+                {
+                    "naam": "s-Gravenzande",
+                    "wijken": ["Centrum", "Edelstenenwijk", "Heenweg", "Nieuwe Vaart", "Oostzijde", "Oudeland", "Teylingen", "Tuinveld", "Vreeburgh", "Vreelande", "Zandevelt", "Het nieuwe water"]
+                },
+                {
+                    "naam": "Naaldwijk",
+                    "wijken": ["Noord", "Oost", "Zuid", "West"]
+                }
+            ]
+        },
+        {
+            "naam": "BAR",
+            "plaatsen": [
+                {
+                    "naam": "Barendrecht",
+                    "wijken": ["Centrum", "Noord", "Binnenland", "Oranjewijk", "Buitenoord", "Ter Leede", "Paddewei", "Molenvliet", "Nieuweland", "Dorp", "Smitshoek", "Voordijk", "Meerwede", "Waterkant", "Havenkwartier", "Gaatkensoog", "Riederhoek", "Vrijhoekakker", "Vrijenburg"]
+                },
+                {
+                    "naam": "Ridderkerk",
+                    "wijken": ["Centrum", "West", "Oost", "Drievliet", "Het Zand", "Slikkerveer", "Bolnes", "Oostendam", "Donkersloot", "Verenambacht", "Cornelisland"]
+                },
+                {
+                    "naam": "Rhoon",
+                    "wijken": ["Centrum", "Oude Koedood", "Noord", "Zuid", "Ghijseland en Huyters", "Overhoeken", "Portland", "Essendael", "Poldergebied"]
+                },
+                {
+                    "naam": "Poortugaal",
+                    "wijken": ["Dorp", "Oost", "Zuid", "West", "Kasteeltuin"]
+                }
+            ]
+        }
+    ]
+}
